33efbea6ee9c93c87d16a05b479052777208780c
stanley <sn859627@gmail.com>
2024-02-02 13:16:00 -0800

    First commit that adds timer.js

---
+++ timer.js
@@ -0,0 +1,22 @@
+/* Create a timer that displays the amount of months, days, 
+hours and seconds until Christmas in real time, this project runs until its Christmas!!!
+*/
+
+setInterval(function () {
+  let now = new Date();
+  let year = now.getFullYear();
+  let christmas = new Date(year, 11, 25);
+  let diff = christmas - now; // Difference in milliseconds
+
+  let seconds = Math.floor(diff / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = Math.floor(hours / 24);
+
+  hours %= 24;
+  minutes %= 60;
+  seconds %= 60;
+  console.log(
+    `Time until Christmas: ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`
+  );
+}, 1000);
